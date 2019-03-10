4904aec9b5a35de5437155f7037a18d604485c76
jzdlsss5 <48313067+jzdlsss5@users.noreply.github.com>
2019-03-10 19:13:24 +0800

    Create AudioManager.cs
    123

---
+++ AudioManager.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;    // 进行控制的Mixer变量
+    public void SetMasterVolume(float volume)    // 控制主音量的函数
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+    public void SetMusicVolume(float volume)    
+    {
+        audioMixer.SetFloat("Music", volume);
+    }
+    public void SetSoundEffectVolume(float volume)   
+    bghfgdgh
+}
